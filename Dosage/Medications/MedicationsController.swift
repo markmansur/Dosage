de0de75da3c15a03ce8057d73835c9c695b87ed7
Mark Mansur <mmansur2@uwo.ca>
2019-12-05 23:36:04 -0500

    remove SwiftUI and setup root vc

---
+++ Dosage/Medications/MedicationsController.swift
@@ -0,0 +1,15 @@
+//
+//  MedicationController.swift
+//  Dosage
+//
+//  Created by Mark Mansur on 2019-12-05.
+//  Copyright © 2019 Mark Mansur. All rights reserved.
+//
+
+import UIKit
+
+class MedicationsController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
