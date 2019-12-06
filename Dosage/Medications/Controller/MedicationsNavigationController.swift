74b7400a5de092168e0750b2c5f3b1032c01c642
Mark Mansur <mmansur2@uwo.ca>
2019-12-06 00:32:24 -0500

    add MedicationNavigationController and refactor folder structure

---
+++ Dosage/Medications/Controller/MedicationsNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  MedicationsNavigationController.swift
+//  Dosage
+//
+//  Created by Mark Mansur on 2019-12-06.
+//  Copyright © 2019 Mark Mansur. All rights reserved.
+//
+
+import UIKit
+
+class MedicationsNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
