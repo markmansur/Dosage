ecff40647c951da45154bff04dd1543171a5df19
Mark Mansur <mmansur2@uwo.ca>
2020-01-08 00:50:56 -0500

    wrap application in MainTabBarController

---
+++ Dosage/MainTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarController.swift
+//  Dosage
+//
+//  Created by Mark Mansur on 2020-01-08.
+//  Copyright © 2020 Mark Mansur. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
